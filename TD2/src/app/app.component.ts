55c96e9fc05ae78fe53c21d701278d0d14fd295a
Brandon <n10895396@qut.edu.au>
2024-06-26 22:26:59 +1000

    That was a lot easier than anticipated...
    Man I'm stupid

--- TD2/src/app/app.component.ts
+++ TD2/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout'
 import { NavigationPreferenceService } from './navigation-preference.service';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { Router, RouterOutlet, RouterModule, NavigationEnd, RouterLink } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import {MatToolbarModule} from '@angular/material/toolbar';
 // import { SwPush } from '@angular/service-worker';
 import { SearchComponent } from './search/search.component';
-import { SidebarNavComponent } from './sidebar-nav/sidebar-nav.component';
 import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 import { Subscription } from 'rxjs';
 import { MatIconModule } from '@angular/material/icon';
@@ -18,7 +18,7 @@ import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SearchComponent, RouterModule, SidebarNavComponent, BottomNavComponent, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule, NgClass],
+  imports: [RouterOutlet, SearchComponent, RouterModule, BottomNavComponent, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule, NgClass, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -27,13 +27,26 @@ export class AppComponent implements OnInit {
   showBottomNav = false;
   showSidebarNav = false;
   isCollapsed = false;
+  activeRoute: string = this.router.url;
   private preferenceSubscription?: Subscription;
 
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private navigationPreferenceService: NavigationPreferenceService
-  ) {}
+    private navigationPreferenceService: NavigationPreferenceService,
+    private router: Router,
+  ) {
+    // animation transition stuff
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(event => {
+        this.activeRoute = (event as NavigationEnd).urlAfterRedirects;
+      });
+  }
+
+   isActiveRoute(route: string): boolean {
+    return this.activeRoute === route;
+  }
 
   ngOnInit() {
   this.breakpointObserver
