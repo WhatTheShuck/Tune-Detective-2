fb7dca5fc46742c77b1198df5d080b51c8999dbd
Brandon <n10895396@qut.edu.au>
2024-06-28 17:54:50 +1000

    Change to form instead of buttons

--- TD2/src/app/export/export-choice/export-choice.component.ts
+++ TD2/src/app/export/export-choice/export-choice.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { FormsModule} from '@angular/forms';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-export-choice',
   standalone: true,
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule, MatButtonToggleModule, FormsModule, ],
   templateUrl: './export-choice.component.html',
   styleUrl: './export-choice.component.css'
 })
 export class ExportChoiceComponent {
-
+  exportContent?: string;
+  exportFormat?: string;
 }
